bf844547da0f12cbce51e9ca1d82f288e34099d7
lunghyun <nangmannsh@gmail.com>
2025-05-01 12:48:57 +0900

    Update WebConfig.java

--- src/main/java/com/hellomello/mock_backend/config/WebConfig.java
+++ src/main/java/com/hellomello/mock_backend/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://34.22.102.29")
+                        .allowedOrigins("http://localhost:3000", "https://test.moongsan.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
